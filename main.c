f60813aaced89ffe9b15d229a0dab54646cb8220
Daniel Martin Ojeda <danielm3@student.42madrid.com>
2025-05-31 13:22:28 +0200

    update

--- main.c
+++ main.c
@@ -6,11 +6,39 @@
 /*   By: danielm3 <danielm3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 11:49:02 by danielm3          #+#    #+#             */
-/*   Updated: 2025/05/31 11:49:25 by danielm3         ###   ########.fr       */
+/*   Updated: 2025/05/31 13:22:03 by danielm3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#include "stagerace.h"
+
+/* int	pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t attr)
+{
+	
+} */
+
+void	racedata_init(t_race_data *racedata)
 {
 	
 }
+/*
+argc = 5
+
+argv[0]: ./stagerace
+argv[1]: number of runners
+argv[2]: number of stages
+argv[3]: min stage time
+argv[4]: max stage time
+
+*/
+
+int	main(int argc, char **argv)
+{
+	t_race_data 	racedata;
+	t_runner_data	*runners_data;
+	
+	racedata_init(t_race_data &racedata);
+	
+}
+
+
